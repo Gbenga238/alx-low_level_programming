53e10f79b24690b21838e7d931f8154c97690538
Gbenga Olajide <aanuoluwapo238@gmail.com>
2022-12-14 07:44:08 +0100

    feat: print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,13 @@
  *
  * Return: Always 0.
  */
- 
+void print_alphabet(void)
+{
+printf("abcdefghijklmnopqrstuvwxyz\n");
+}
 
 int main(void)
 {
     print_alphabet();
     return (0);
-}
-void print_alphabet(void)
-{
-printf("abcdefghijklmnopqrstuvwxyz\n");
 }
