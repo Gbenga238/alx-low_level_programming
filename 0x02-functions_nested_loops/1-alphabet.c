b8940255f55aa2ff85e63a57fc06e4cf0cafac71
Gbenga Olajide <aanuoluwapo238@gmail.com>
2022-12-14 07:56:31 +0100

    feat: print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,13 +5,15 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
-{
-printf("abcdefghijklmnopqrstuvwxyz\n");
-}
+void print_alphabet(void);
+
 
 int main(void)
 {
     print_alphabet();
     return (0);
+}
+void print_alphabet(void)
+{
+printf("abcdefghijklmnopqrstuvwxyz\n");
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,5 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 #include <stdio.h>
-void print_alphabet(void);
 
 #endif
