e7b717efd88c235efd690aa75ab4eab7887385ef
Gbenga Olajide <aanuoluwapo238@gmail.com>
2022-12-20 19:57:36 +0100

    update:new code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - prints the alphabet in lowercase
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
-void print_alphabet(void);
 
-int main()
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
-}
-void print_alphabet()
-{
-printf("abcdefghijklmnopqrstuvwxyz\n");
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
 }
