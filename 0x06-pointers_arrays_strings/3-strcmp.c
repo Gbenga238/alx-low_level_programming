1ea09fa114a76be75c24f3e6f3fc4ce315d8e909
Gbenga Olajide <aanuoluwapo238@gmail.com>
2022-12-22 09:01:17 -0800

    update: betty style fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,5 @@
 #include "main.h"
-
 /**
-This function compares two strings and returns the difference between their first unmatched characters. If the first string is less than the second, the return value will be negative. If the two strings are equal, the return value will be 0. If the first string is greater than the second, the return value will be positive. The function takes in two pointers to the strings being compared, s1 and s2
-
  * _strcmp - Compares pointers to two strings.
  * @s1: A pointer to the first string to be compared.
  * @s2: A pointer to the second string to be compared.
